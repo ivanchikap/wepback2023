e66246f1da9532bf1b6e8787da357354b4ed2fc0
ivanchikap <anton.ivanchik@gmail.com>
2023-03-31 10:38:12 +0300

    add handlebars: shablonizator

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,9 @@
-console.log('it is test page');
+import itemsTemplate from '../templates/gallery-items.hbs';
+
+
+// const markup = itemsTemplate({items: ['html', 'css', 'js', 'react']});
+const markup = itemsTemplate(['html', 'css', 'js', 'react']);
+
+const gallery = document.querySelector('.js-gallery');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
--- webpack.config.js
+++ webpack.config.js
@@ -116,6 +116,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.hbs$/,
+        use: 'handlebars-loader'
+      }
     ],
   },
 };
